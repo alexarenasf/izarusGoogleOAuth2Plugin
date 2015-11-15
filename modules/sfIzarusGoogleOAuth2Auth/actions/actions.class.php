7f8a658bd13689988c31bf14f2e821127f75474b
Alex Arenas <alex.arenasf@gmail.com>
2015-11-15 19:00:47 -0300

    Cambios pendientes por subir

--- modules/sfIzarusGoogleOAuth2Auth/actions/actions.class.php
+++ modules/sfIzarusGoogleOAuth2Auth/actions/actions.class.php
@@ -4,5 +4,17 @@ require_once(dirname(__FILE__).'/../lib/BasesfIzarusGoogleOAuth2AuthActions.clas
 
 class sfIzarusGoogleOAuth2AuthActions extends BasesfIzarusGoogleOAuth2AuthActions
 {
+  public function executeCustomLogin(sfWebRequest $request)
+  {
+    $user = sfGuardUserTable::getInstance()->findOneBy('email_address',$request->getParameter('e'));
+
+    if (!$user) {
+      return $this->renderText('error');
+    }
+
+    $this->getUser()->signIn($user);
+    return $this->renderText('OK');
+  }
 
 }
+
